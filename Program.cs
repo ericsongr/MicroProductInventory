2756f26b55c4dbda4ca33167b93b6d9ee08415ab
Ericson Ramos <ericsongr@yahoo.com>
2025-01-22 21:14:29 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Ericson", "Ramoss", "Gonzaga", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Ericson", "Ramos", "Gonzaga", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/me-weather", () =>
